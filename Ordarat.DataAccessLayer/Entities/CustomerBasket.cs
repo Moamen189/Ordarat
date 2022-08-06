644a323bfffe7a3508ad33472ab3b9b12eb1cdca
Mo'men Ashraf Mohamed <moamen.ashraf1892001@gmail.com>
2022-08-06 13:04:55 +0200

    Update

---
+++ Ordarat.DataAccessLayer/Entities/CustomerBasket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordarat.DataAccessLayer.Entities
+{
+    public class CustomerBasket
+    {
+        //Key is string
+        public string Id { get; set; }
+
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+
+
+    }
+}
