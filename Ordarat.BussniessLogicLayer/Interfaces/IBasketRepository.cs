40c7d0f07c97a2dec2658bc22c1df488d8958fed
Mo'men Ashraf Mohamed <moamen.ashraf1892001@gmail.com>
2022-08-06 15:07:56 +0200

    Redis

---
+++ Ordarat.BussniessLogicLayer/Interfaces/IBasketRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ordarat.DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordarat.BussniessLogicLayer.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetCustomerBasket(string basketId);
+
+        Task<CustomerBasket> UpdateCustomerBasket(CustomerBasket basket);
+
+        Task<bool> DeleteCustomerBasket(string basketId);
+
+    }
+}
---
+++ Ordarat.BussniessLogicLayer/Repository/BasketRepository.cs
@@ -0,0 +1,41 @@
+﻿using Ordarat.BussniessLogicLayer.Interfaces;
+using Ordarat.DataAccessLayer.Entities;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Ordarat.BussniessLogicLayer.Repository
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDatabase _database;
+
+        public BasketRepository(ConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+        public async Task<bool> DeleteCustomerBasket(string basketId)
+        {
+            return await _database.KeyDeleteAsync(basketId);
+        }
+
+        public async Task<CustomerBasket> GetCustomerBasket(string basketId)
+        {
+            var basket = await _database.StringGetAsync(basketId);
+            return  basket.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(basket);
+        }
+
+        public async Task<CustomerBasket> UpdateCustomerBasket(CustomerBasket basket)
+        {
+            var created = await _database.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+            if (!created)
+                return null;
+            return await GetCustomerBasket(basket.Id);  
+            
+        }
+    }
+}
