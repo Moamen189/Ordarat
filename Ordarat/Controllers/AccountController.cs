e61c3dcb29b31a5a706eba16ca5f04ddb7cf530d
Mo'men Ashraf Mohamed <moamen.ashraf1892001@gmail.com>
2022-08-07 17:29:44 +0200

    LoginEndPoint

---
+++ Ordarat/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Ordarat.DataAccessLayer.Entities.Identity;
+using Ordarat.Dtos;
+using Ordarat.Errors;
+using System.Threading.Tasks;
+
+namespace Ordarat.Controllers
+{
+   
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager , SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if(user == null)
+                return Unauthorized(new ApiResponse(401));
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if(!result.Succeeded)
+                return Unauthorized(new ApiResponse(401));
+
+            return Ok(new UserDto()
+            {
+
+                DisplayName = user.DisplayName,     
+                Email = user.Email,
+                Token = "This Will Be Token"
+            });
+
+
+
+
+        }
+    }
+}
---
+++ Ordarat/Dtos/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordarat.Dtos
+{
+    public class UserDto
+    {
+        public string DisplayName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Token { get; set; }
+    }
+}
