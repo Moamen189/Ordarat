0a2054dba61ebb31e66350fb743ea117954bba5d
Mo'men Ashraf Mohamed <moamen.ashraf1892001@gmail.com>
2022-08-13 17:18:01 +0200

    DeliveyMConfig

---
+++ Ordarat.DataAccessLayer/Data/Config/DeliveryMethodConfigration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordarat.DataAccessLayer.Entities.Order_Aggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordarat.DataAccessLayer.Data.Config
+{
+    internal class DeliveryMethodConfigration : IEntityTypeConfiguration<DelivaryMethod>
+    {
+        public void Configure(EntityTypeBuilder<DelivaryMethod> builder)
+        {
+            builder.Property(DM => DM.Cost)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
