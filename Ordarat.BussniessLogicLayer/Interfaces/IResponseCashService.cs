78fe2a24619bb2ec64adfdc4432ebe8865283379
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-08-30 21:49:50 +0200

    InterFaceCasheServices

---
+++ Ordarat.BussniessLogicLayer/Interfaces/IResponseCashService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordarat.BussniessLogicLayer.Interfaces
+{
+    public interface IResponseCashService
+    {
+        Task CasheResponseAsync(string cacheKey , object response , TimeSpan timeToLive );
+        Task<string> GetCashedResponse(string cacheKey);
+    }
+}
