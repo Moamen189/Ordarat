1fa869ed7cb45389a0cc58d32be9c458e06279db
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-08-31 17:04:09 +0200

    CashedResponse

--- Ordarat/Helpers/CashedResponse.cs
+++ Ordarat/Helpers/CashedResponse.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Threading.Tasks;
 
 namespace Ordarat.Helpers
 {
-    public class CashedResponse:Attribute
+    public class CashedResponse : Attribute, IAsyncActionFilter
     {
+        private readonly int _timeToLiveInSecond;
+
+        public CashedResponse(int timeToLiveInSecond)
+        {
+            _timeToLiveInSecond = timeToLiveInSecond;
+        }
+        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
